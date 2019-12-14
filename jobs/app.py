98534cf4fd830259ba605276924ff13a7eab1b1b
SBakshi2 <sbaksi2@slb.com>
2019-12-14 13:05:25 +0530

    added the sqilte connection

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+
+PATH = "db\jobs.sqlite"
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection')
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
 
 @app.route('/')
 @app.route('/jobs')
