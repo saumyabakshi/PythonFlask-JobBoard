a0bdcfa39d9839610622222ed634b46bec0a611b
SBakshi2 <sbaksi2@slb.com>
2019-12-14 18:06:29 +0530

    made pep8 standards compatible

--- jobs/app.py
+++ jobs/app.py
@@ -2,9 +2,10 @@ import sqlite3
 from flask import Flask, render_template, g
 
 
-PATH = "db\jobs.sqlite"
+PATH = 'db\jobs.sqlite'
 
 app = Flask(__name__)
+
 
 def open_connection():
     connection = getattr(g, '_connection')
@@ -12,6 +13,7 @@ def open_connection():
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
+
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -22,6 +24,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = cursor.fetchone() if single else cursor.fetchall()
     cursor.close()
     return results
+
 
 @app.teardown_appcontext
 def close_connection(exception):
